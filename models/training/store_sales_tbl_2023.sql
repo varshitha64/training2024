509bec4a1a4a1efc292212c9e144b8e4682ae792
Thudumu varshitha <thudumuvarshitha@gmail.com>
2024-10-01 08:27:00 +0000

    commit changes

--- models/training/store_sales_tbl_2023.sql
+++ models/training/store_sales_tbl_2023.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
-with store_Sales_tbl_2023 as
+with store_sales_tbl_2023 as
 (
-select SS_STORE_SK,SS_QUANTITY,SS_SALES_PRICE,SS_NET_PROFIT  from dbtsf_sc.Store_Sales_2023
-)
-select * from store_Sales_tbl_2023
+select SS_STORE_SK,SS_QUANTITY,SS_SALES_PRICE,SS_NET_PROFIT  from {{ source('test_DBTSF_SC', 'store_sales_2023')}}
+
+select * from store_sales_tbl_2023
--- models/training/store_tbl_2023.sql
+++ models/training/store_tbl_2023.sql
@@ -2,6 +2,7 @@
 
 with store_tbl_2023 as
 (
-select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from dbtsf_sc.Store_2023
+select S_STORE_SK,S_STORE_ID,S_STORE_NAME,S_COUNTY from {{ source('test_DBTSF_SC', 'store_2023')}}
+
 )
 select * from store_tbl_2023
